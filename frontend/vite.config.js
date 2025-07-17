252a60bc453b03738e760bb628881b357bb3d4bb
DEVprasad7 <maskar.prasad.comp@kgce.edu.in>
2025-07-17 19:39:56 +0530

    Reverting the changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 
-
-export default defineConfig((command, mode) => {
-  const env = loadEnv(mode, process.cwd(), "")
-
-  console.log(env.VITE_DEBUG)
-
-  return {
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
-
